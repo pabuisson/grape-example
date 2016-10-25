1e43f45e485e448bcc53f4f6d035736a2b7aabf6
Pierre-Adrien <pabuisson@gmail.com>
2016-10-25 20:44:51 +0200

    Remove the url + apikey text fields from the documentation header

--- config/initializers/grape_swagger_rails.rb
+++ config/initializers/grape_swagger_rails.rb
@@ -1,4 +1,8 @@
 GrapeSwaggerRails.options.url      = '/swagger_doc'
 GrapeSwaggerRails.options.app_name = 'Music API'
 GrapeSwaggerRails.options.app_url  = 'http://api.lvh.me:3000'
+
+GrapeSwaggerRails.options.hide_api_key_input = true
+GrapeSwaggerRails.options.hide_url_input     = true
+
 GrapeSwaggerRails.options.headers['X-Api-Key'] = '565d34efaf32'
